15eb963d0acb317d49cbf32fcbd0289507e0c08d
DaveJosef <josedaaaaavid@gmail.com>
2022-03-25 21:53:19 -0300

    :construction: Modelo de Intermediação de Requisições

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const db = require("./database/config");
 
+const {ROUTES} = require('./configuration/routes');
+const {setupProxies} = require('./configuration/proxy');
+
 db.sync();
 
 const commentsRoutes = require("./routes/comments.routes");
@@ -9,5 +12,7 @@ const commentsRoutes = require("./routes/comments.routes");
 app.use(express.json());
 
 app.use("/comments", commentsRoutes);
+
+setupProxies(app, ROUTES);
 
 module.exports = app;
