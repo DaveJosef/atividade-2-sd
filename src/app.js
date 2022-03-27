ed839dcc36afb62822ce32336ce6addcc8f26472
José Ricardo <jricardobgoncalves@gmail.com>
2022-03-27 18:06:18 -0300

    :recycle: Remove API Gateway model

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const db = require("./database/config");
 
-const {ROUTES} = require('./configuration/routes');
-const {setupProxies} = require('./configuration/proxy');
-
 db.sync();
 
 const commentsRoutes = require("./routes/comments.routes");
@@ -12,7 +9,5 @@ const commentsRoutes = require("./routes/comments.routes");
 app.use(express.json());
 
 app.use("/comments", commentsRoutes);
-
-setupProxies(app, ROUTES);
 
 module.exports = app;
